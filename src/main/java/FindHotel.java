914d1f63ceaf12c6bc5c20608eb69fdca12fd745
Andrei Budevich <budevichac01@gmail.com>
2022-06-03 21:04:11 +0300

    add deserializeToXML and sortedHotels

--- src/main/java/FindHotel.java
+++ src/main/java/FindHotel.java
@@ -1,4 +1,15 @@
+import model.Hotel;
+import util.HotelUtil;
+import util.XmlUtil;
+
+import java.util.List;
+
 public class FindHotel {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        String path = args[0];
+
+        List<Hotel> allHotels = XmlUtil.deserializeToXML(path);
+
+        List<Hotel> sortedHotels =  HotelUtil.getSorted(allHotels, "Hilton", "New York");
     }
 }
