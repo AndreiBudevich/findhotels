df4807a5a4f7529702ede1865a2f447f1e57c4cb
Andrei Budevich <budevichac01@gmail.com>
2022-06-03 22:40:18 +0300

    add report, reportSerialize

--- src/main/java/FindHotel.java
+++ src/main/java/FindHotel.java
@@ -1,15 +1,23 @@
 import model.Hotel;
+import model.Hotels;
+import to.Report;
 import util.HotelUtil;
 import util.XmlUtil;
 
 import java.util.List;
 
 public class FindHotel {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String path = args[0];
 
-        List<Hotel> allHotels = XmlUtil.deserializeToXML(path);
+        List<Hotel> allHotels = XmlUtil.deserializeToXML(path, Hotels.class).getHotels();
 
-        List<Hotel> sortedHotels =  HotelUtil.getSorted(allHotels, "Hilton", "New York");
+        List<Hotel> sortedHotels = HotelUtil.getSorted(allHotels, "Hilton", "New York");
+
+        Report report = HotelUtil.get(sortedHotels);
+
+        String reportSerialize = XmlUtil.serializeToXML(report, Report.class);
+
+        System.out.println(reportSerialize);
     }
 }
